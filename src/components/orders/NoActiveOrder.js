b6bb940b719481112568b399a7d529f5951d513d
Amr Elsekilly <github@amrsekilly.com>
2019-01-12 19:58:24 +0200

    Changed the Kawaii color to Rubikal's theme

--- src/components/orders/NoActiveOrder.js
+++ src/components/orders/NoActiveOrder.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Mug } from 'react-kawaii';
+import yellow from '@material-ui/core/colors/yellow';
 
 export default class NoActiveOrder extends Component {
   render() {
     return (
       <div>
         <h2>You don't have any active orders!</h2>
-        <Mug size={170} mood="shocked" color="#A6E191" />
+        <Mug size={170} mood="shocked" color={yellow.A700} />
       </div>
     )
   }
