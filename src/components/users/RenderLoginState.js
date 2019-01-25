7974a2eb4c233d37e98c89bb028c9875ae13d4b9
Amr Elsekilly <github@amrsekilly.com>
2019-01-25 18:51:10 +0200

    Added the user avatar

--- src/components/users/RenderLoginState.js
+++ src/components/users/RenderLoginState.js
@@ -3,22 +3,42 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Avatar from '@material-ui/core/Avatar';
 
 const styles = {
   linkStyles: {
     color: 'inherit',
     textDecoration: 'none'
-  }
+  },
+   bigAvatar: {
+     margin: 10,
+     width: 60,
+     height: 60,
+   },
 };
 
 class RenderLoginState extends Component {
   renderLoginState = () => {
-    const {loginState} = this.props;
+    const {
+      loginState,
+      avatar,
+      userName
+    } = this.props;
+
     if (loginState === 'inProgress') {
       return (
         <Button color="inherit" disabled>
           <CircularProgress size={24} color="secondary" />
         </Button>
+      );
+    } else if(loginState === 'loggedIn') {
+      return (
+        <IconButton color="inherit">
+          <Avatar alt={userName} src={avatar} className={styles.bigAvatar} />
+        </IconButton>
       );
     }
     else {
@@ -41,7 +61,9 @@ class RenderLoginState extends Component {
 }
 
 const mapStateToProps = state => ({
-    loginState: state.getIn(['users', 'loginState'])
+    loginState: state.getIn(['users', 'loginState']),
+    avatar: state.getIn(['users', 'userInfo', 'avatar']),
+    userName: state.getIn(['users', 'userInfo', 'name']),
 });
 
 const mapDispatchToProps = {
