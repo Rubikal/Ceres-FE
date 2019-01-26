597d450f9ea0c35b2575d51f303cf6e0ff541ab5
Amr Elsekilly <github@amrsekilly.com>
2019-01-27 00:47:52 +0200

    Updated the noActive order page to use MaterialUI

--- src/components/orders/NoActiveOrder.js
+++ src/components/orders/NoActiveOrder.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { IceCream } from 'react-kawaii';
 import yellow from '@material-ui/core/colors/yellow';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-export default class NoActiveOrder extends Component {
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  kawaii: {
+    paddingTop: theme.spacing.unit * 4,
+  }
+});
+
+class NoActiveOrder extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        <h2>You don't have any active orders!</h2>
-        <IceCream size={270} mood="shocked" color={yellow.A700} />
+        <Paper className={classes.root} elevation={0}>
+          <Typography variant="h5" component="h3">
+           You don't have any active orders!
+          </Typography>
+          <div className={classes.kawaii}>
+            <IceCream size={270} mood="shocked" color={yellow.A700} />
+          </div>
+        </Paper>
       </div>
-    )
+    );
   }
 }
+
+NoActiveOrder.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NoActiveOrder);
